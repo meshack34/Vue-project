534e60a980c6bf0bc0caaa5839b5c8ffbdf3fd53
meshack34 <meshackkimutai34@gmail.com>
2022-05-12 13:19:38 +0300

    solve theme color

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ const app = Vue.createApp({
         }
     },
     methods:{
-        getUser(){
-            this.firstname: "meshack",
-            this.lastname:'kimutai',
-            this.email:"meshack34@gmail.com",
-            this.gender:"female",
-            this.picture:"https://randomuser.me/api/portraits/men/10.jpg",
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            console.log(results)
+            this.firstname = results[0].name.first
+            this.lastname =results[0].name.last
+            this.email=results[0].email
+            this.gender=results[0].gender
+            this.picture= results[0].picture.large
         },
     },
 
